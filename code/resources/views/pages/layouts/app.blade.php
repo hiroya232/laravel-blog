aa225e5733acbee5cd39f2340bb125760580ee75
hiroya-akeoka <mowoarm232@gmail.com>
2022-01-17 22:08:23 +0900

    :recycle:layoutsコンポーネントの不要なコメントを削除

--- code/resources/views/pages/layouts/app.blade.php
+++ code/resources/views/pages/layouts/app.blade.php
@@ -8,13 +8,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
@@ -22,7 +17,6 @@
     <div class="min-h-screen bg-gray-100">
         @include('pages.layouts.navigation')
 
-        <!-- Page Content -->
         <main>
             @yield('content')
         </main>
